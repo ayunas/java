25f4fc7f88d9793f350d0a98bcfe5db09097bb17
Amir Yunas <ayunas@gmail.com>
2020-05-05 18:16:34 -0400

    streams api practice imtiaz ahmed

--- lambda_streams/src/jobreadyprogrammer/car/App.java
+++ lambda_streams/src/jobreadyprogrammer/car/App.java
@@ -5,6 +5,8 @@ import org.w3c.dom.ls.LSOutput;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.locks.Condition;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class App {
     static List<Car> cars = Arrays.asList(
@@ -30,15 +32,41 @@ public class App {
 //            }
 //        });
 
+        printCars(new CarConditionable(){
+            @Override
+            public boolean test(Car c) {
+                return c.color == "green";
+            }
+        });
+
         printCars(c -> c.color == "green");
 
         printCars(c -> c.price >= 30000 && c.price <= 50000);
+
+        printGeneric(c -> c.make == "Toyota");
+
+        printCarLib(c -> c.color == "green" && c.price < 100000);
+
+        Function<Car,String> printPriceColor = c -> "price : " + c.price + " color : " + c.color;
+
+        System.out.println(printPriceColor.apply(cars.get(0)));
+
 
     }
 
     static void printGeneric(Conditionable<Car> condition) {
         for (Car car : cars) {
+            if(condition.test(car)) {
+                System.out.println(car);
+            }
+        }
+    }
 
+    static void printCarLib(Predicate<Car> predicateCondition) {
+        for(Car c : cars) {
+            if(predicateCondition.test(c)) {
+                System.out.println(c);
+            }
         }
     }
 
